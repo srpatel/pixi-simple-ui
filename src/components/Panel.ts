d101f450a23760b56a3235efedabe53674a065fc
sunil <sunil@xikka.com>
2024-11-27 10:28:03 +0100

    add default props to Panel

--- src/components/Panel.ts
+++ src/components/Panel.ts
@@ -1,28 +1,36 @@
 import * as PIXI from "pixi.js";
 import Component from "./Component";
 
+type PanelProps = {
+  width: number;
+  height: number;
+  rounded: "sm" | "md" | "lg";
+  color: PIXI.ColorSource;
+};
+
 export default class Panel extends Component {
   private pane: PIXI.NineSliceSprite;
-  constructor(
-    props?: Partial<{
-      width: number;
-      height: number;
-      rounded: "sm" | "md" | "lg";
-      color: PIXI.ColorSource;
-    }>
-  ) {
+  public static defaultProps: PanelProps = {
+    width: 100,
+    height: 100,
+    rounded: "md",
+    color: 0xffffff,
+  };
+
+  constructor(props?: Partial<PanelProps>) {
     super();
 
+    props = {
+      ...Panel.defaultProps,
+      ...(props ?? {}),
+    };
+
     this.pane = new PIXI.NineSliceSprite(
-      PIXI.Texture.from(`simple-ui/rounded-${props?.rounded ?? "md"}.png`)
+      PIXI.Texture.from(`simple-ui/rounded-${props.rounded}.png`)
     );
     this.addChild(this.pane);
-
-    this.setSize(props?.width, props?.height);
-
-    if (props?.color != undefined) {
-      this.setColor(props?.color);
-    }
+    this.setSize(props.width, props.height);
+    this.setColor(props.color);
   }
 
   setColor(color: PIXI.ColorSource) {
